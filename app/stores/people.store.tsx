a57b8d4667f7b73a8e49ba9607e94757e4b64e3e
yauhen_penkin <e.penkin@akveo.com>
2018-06-06 14:14:22 +0300

    feat(app): person model add, people store add

---
+++ app/stores/people.store.tsx
@@ -0,0 +1,12 @@
+import { observable, action } from 'mobx';
+import Person from '../models/models';
+
+export default class PeopleStore {
+
+  @observable people: Person[] = [];
+
+  @action
+  setPeople(people: Person[]) {
+    this.people = people;
+  }
+}
